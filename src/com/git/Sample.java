06ccc13848eb1eb4169954d1a9501f33e6300c67
ranavee <ranavee2022@gmail.com>
2023-01-05 13:11:12 +0530

    StringTask

---
+++ src/com/git/Sample.java
@@ -0,0 +1,21 @@
+package com.git;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		String s = "Git Trail";
+		String output = "";
+		String[] split = s.split(" ");
+
+		for (String a : split) {
+			String rev = "";
+
+			for (int i = a.length() - 1 ; i>=0 ; i--) {
+				rev = rev + a.charAt(i);
+			}
+
+			System.out.print(output = rev + " ");
+		}
+	}
+
+}
